4b00ce6be29a74861839fb8138590b425f062454
Shanmugam Ramasamy <111910568+shanmugamr1992@users.noreply.github.com>
2023-09-25 16:06:59 -0700

    Update run_robot.py

--- run_robot.py
+++ run_robot.py
@@ -1,8 +1,9 @@
 import cv2
 from robot import Robot
 import time
+import sys
 
 if __name__ == "__main__":
-  r = Robot(run_on_nano=False)
-  time.sleep(4)
+  r = Robot(run_on_nano=sys.argv[1])
+  time.sleep(2)
   r.run()
